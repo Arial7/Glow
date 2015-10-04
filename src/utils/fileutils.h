2896e5c9af82b13a690d05af560f3d5fe95b1344
Arial7 <riesinger.pascal@gmail.com>
2015-10-04 19:18:24 +0000

    fixed fileutils not compiling

--- src/utils/fileutils.h
+++ src/utils/fileutils.h
@@ -9,11 +9,12 @@ namespace Glow { namespace utils {
             std::fstream stream_;
             std::string path_;
         public:
-            File(std::string path){
-                stream_ = std::fstream(path, std::fstream::in |
-                    std::fstream::out | std::fstream::app);
+            File(std::string path) : stream_(path, std::fstream::in | 
+                std::fstream::out | std::fstream::app){
                 path_ = path;
             };
+            //TODO: reimplement this function (not very important) 
+            /*
             File(std::string path, bool overwrite) {
                 if(overwrite)
                     stream_ = std::fstream(path, std::fstream::in |
@@ -23,7 +24,7 @@ namespace Glow { namespace utils {
                         std::fstream::out | std::fstream::app);
                 path_ = path;
             };
-
+            */
             ~File(){stream_.close();};
             std::string read();
             bool write(std::string content);
